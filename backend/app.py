2dc2fd949a2b6100680e05370f061f1eb442c169
LynnKaafarani20 <lynn.kaafarani01@lau.edu>
2026-04-30 22:56:54 +0300

    Fix app.run indentation

--- backend/app.py
+++ backend/app.py
@@ -830,7 +830,5 @@ def register_recipient():
 
     return jsonify({"success": True, "user_id": user_id, "recipient_id": recipient_id})
 if __name__ == "__main__":
-
-
     port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
