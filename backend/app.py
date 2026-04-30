abeb46041c5c16c6faf2c905806ee4770dcaec15
LynnKaafarani20 <lynn.kaafarani01@lau.edu>
2026-04-30 22:22:12 +0300

    Fix port for deployment

--- backend/app.py
+++ backend/app.py
@@ -830,4 +830,7 @@ def register_recipient():
 
     return jsonify({"success": True, "user_id": user_id, "recipient_id": recipient_id})
 if __name__ == "__main__":
-    app.run(debug=Config.DEBUG, port=5000)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
