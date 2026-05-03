68787f5ab80e24cb8af19bf3850d119dbb0aa121
Jana-Monzer <janamonzer95@gmail.com>
2026-05-03 03:22:20 +0300

    update app

--- backend/app.py
+++ backend/app.py
@@ -301,4 +301,4 @@ def register_donation():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)git status
+    app.run(host="0.0.0.0", port=port)
